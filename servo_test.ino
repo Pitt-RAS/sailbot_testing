21f1d4d6d19524883836ba0f4cf71f1ebb7d0898
Jeremy Manin <jmm350@pitt.edu>
2017-10-05 23:35:16 -0400

    Added Stepper Control

--- servo_test.ino
+++ servo_test.ino
@@ -1,17 +1,22 @@
 #include <Servo.h>
+#include <Stepper.h>
+
+const int SPR= 200;
 
 Servo myservo;
+Stepper mystepper(SPR, 8, 9, 10, 11);
 int pos= 0;
 byte incomingByte;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);
+  myservo.attach(6);
 }
 
 void loop() {
   if(Serial.available()>0){
     incomingByte= Serial.read();
+  
     if(incomingByte=='.' && pos<=178){
       pos+=2;
       myservo.write(pos);
@@ -20,6 +25,11 @@ void loop() {
       pos-=2;
       myservo.write(pos);
     }
+    else if(incomingByte=='w'){
+      mystepper.step(2);
+    }
+    else if(incomingByte=='s'){
+      mystepper.step(-2);
+    }
   }
-  Serial.print(pos);
 }
