3d8cd2e2db663a14d31700ae9ee7beff15ad2b48
nateriek <32182835+nateriek@users.noreply.github.com>
2017-10-12 19:44:04 -0400

    Add files via upload

---
+++ Servo.ino
@@ -0,0 +1,32 @@
+
+#include <Servo.h>
+#include <Stepper.h>
+
+Servo myservo;    
+Stepper myStepper(SPR,8,9,10,11);
+const int SPR = 200;
+int pos = 0; 
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(6);  
+}
+
+void loop() {
+  char c = Serial.read();
+  if (c == 'l' && pos!=0){
+    pos-=5;
+  }
+  if (c == 'r' && pos!= 180){
+      pos+=5;
+  }
+  if (c == 'a'){
+    myStepper.step(-1);
+  }
+  if(c == 's'){
+    myStepper.step(1)
+  }
+    myservo.write(pos);    
+  
+}
+
