555f97dc705e378d80a3cca6de560fc80e955115
nateriek <nateriek98@gmail.com>
2017-11-18 17:40:37 -0500

    Added Rudder Control code

---
+++ rudderControl/rudderControl.ino
@@ -0,0 +1,31 @@
+/* 
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
+ */
+
+#include <ros.h>
+#include <Servo.h>
+#include <std_msgs/Int32.h>
+
+Servo myservo;
+ros::NodeHandle  nh;
+
+void messageCb( const std_msgs::Int32& rudderHeading){
+  myservo.write(int(rudderHeading));
+}
+
+ros::Subscriber<std_msgs::Int32> sub("new_rudder_heading", &messageCb );
+
+void setup()
+{ 
+  myservo.attach(6);
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{  
+  nh.spinOnce();
+  delay(1);
+}
+
