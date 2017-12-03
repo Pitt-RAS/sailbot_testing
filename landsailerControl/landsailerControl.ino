0d3561b8eb6322196a2b1ca46d3cd20358850c61
jmmanin <jmm350@pitt.edu>
2017-12-02 22:06:17 -0500

    added missing library and sub

--- landsailerControl/landsailerControl.ino
+++ landsailerControl/landsailerControl.ino
@@ -4,9 +4,10 @@
  */
 
 #include <ros.h>
-#include <Servo.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
+#include <Servo.h>
+#include <Stepper.h>
 
 const int SPR= 200;
 
@@ -20,20 +21,22 @@ void rudderCb(const std_msgs::Int32 &rudderHeading){
   myservo.write(rudderHeading.data); //changes rudder heading
 }
 
-void sailCb(const std_msgs::Float32 &sailAngle){
-  int temp= (int)map(sailAngle.data, 0, 90, 0, 4500);
+void sailCb(const std_msgs::Int32 &sailAngle){
+  int temp= map(sailAngle.data, 0, 90, 0, 4500);
   mystepper.step(temp-sheet);
   sheet= sheet + (temp-sheet);
 }
 
-ros::Subscriber<std_msgs::Int32> sub("new_rudder_heading", &rudderCb );
+ros::Subscriber<std_msgs::Int32> rudderSub("cmd_rudder_angle", &rudderCb );
+ros::Subscriber<std_msgs::Int32> sailSub("cmd_sail_angle", &sailCb );
 
 void setup()
 { 
   mystepper.setSpeed(60);
   myservo.attach(6);
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(rudderSub);
+  nh.subscribe(sailSub);
 }
 
 void loop()
