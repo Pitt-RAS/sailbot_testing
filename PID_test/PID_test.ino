6319f1da44c3148f201cbb2c3702362b948e4378
djrowe79 <djr79@pitt.edu>
2018-03-24 18:10:13 -0400

    Fixed Jittery issue

--- PID_test/PID_test.ino
+++ PID_test/PID_test.ino
@@ -1,3 +1,4 @@
+
 #include "pid.h"
 #include <Servo.h>
 
@@ -9,9 +10,6 @@
 Servo myservo;
 PID pid = PID(Kp, Kd, Ki);
 
-String inputString = "";
-bool outputting = true;
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(pwmOut_pin, OUTPUT);
@@ -20,34 +18,24 @@ void setup() {
 
   pid.setSetpoint(140);
 
-  Serial.print("Started");
+  Serial.println("Started");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //delay(10);
   int potValue = analogRead(A0); // Read potentiometer value
-  int motorAngle = map(potValue, 0, 1023, 0 , 280); // Map the potentiometer value from 0 to 280
+  double motorAngle = map(potValue, 0, 1023, 0 , 280); // Map the potentiometer value from 0 to 280
   double pidOut = pid.calculate(motorAngle);
   double spd = 90;
-  if(pidOut > 250){
+  if(pidOut >= 250){
     spd = 180;
-  } else if(pidOut < -250){
+  } else if(pidOut <= -250){
     spd = 0;
   } else {
     spd = map(pidOut, -250, 250, 0, 180); 
   }
-  
-  /*
-  //This way should work but it doesn't (T^T)
-  double spd = pidOut/4 + 90;
-  if(spd > 180){
-    spd = 180;
-  }
-  if(spd < 0){
-    spd = 0;
-  }
-  */
+
+  delay(1);
   myservo.write(spd);
 }
 
